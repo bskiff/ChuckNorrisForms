b984c3f389f9e8ca36370b50bf9ef7b3eddff913
Ben Skiff <bskiff@sourceallies.com>
2015-04-19 15:10:24 -0500

    Initial Commit

---
+++ ChuckNorrisForms/ChuckNorrisForms/QuoteViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ChuckNorrisForms
+{
+    class QuoteViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private String _currentQuote;
+
+        public String CurrentQuote
+        {
+            get { return _currentQuote ?? String.Empty; }
+            set
+            {
+                if (_currentQuote != value)
+                {
+                    _currentQuote = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("CurrentQuote"));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ChuckNorrisForms/ChuckNorrisForms/WelcomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ChuckNorris;
+using Xamarin.Forms;
+
+namespace ChuckNorrisForms
+{
+    public partial class WelcomePage : ContentPage
+    {
+        private readonly ChuckNorrisQuotes _chuckNorrisQuotes = new ChuckNorrisQuotes();
+        private readonly QuoteViewModel _quoteViewModel = new QuoteViewModel();
+
+        public WelcomePage()
+        {
+            InitializeComponent();
+            _quoteViewModel.CurrentQuote = GetNextQuote();
+            BindingContext = _quoteViewModel;
+//            BindingContext = _quoteViewModel;
+        }
+
+        private string GetNextQuote()
+        {
+            return _chuckNorrisQuotes.GetNextQuote();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            _quoteViewModel.CurrentQuote = GetNextQuote();
+        }
+    }
+}
